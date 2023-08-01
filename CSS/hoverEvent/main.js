5d05ab660ba2f4b37f3c4a4577abc5360c298c44
Kari Smith <kariasmith@hotmail.com>
2023-08-01 10:19:37 -0600

    Add files via upload

---
+++ CSS/hoverEvent/main.js
@@ -0,0 +1,16 @@
+/*function over(element) {
+    alert("mouseover");    
+}
+    
+function out(element) {
+    alert("mouseout");    
+}
+*/
+
+function over(element) {
+    element.style.backgroundColor = "lime";    
+}
+    
+function out(element) {
+    element.style.backgroundColor = "silver";   
+}
