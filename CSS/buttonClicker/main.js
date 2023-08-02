80504443f5172e22fc125af4ae60acfd5fbc644b
Kari Smith <kariasmith@hotmail.com>
2023-08-02 01:32:11 -0600

    Add files via upload

---
+++ CSS/buttonClicker/main.js
@@ -0,0 +1,15 @@
+console.log("Running main.js")
+
+function logout(element) {
+    element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+function increase(element) {
+    element.innerText = parseInt(element.innerText) + 1;
+    
+}
+
