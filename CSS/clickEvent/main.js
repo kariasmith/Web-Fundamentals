12f15a13de17fd073f661c80a910722779314db8
Kari Smith <kariasmith@hotmail.com>
2023-08-03 20:23:34 -0600

    Add files via upload

---
+++ CSS/clickEvent/main.js
@@ -0,0 +1,13 @@
+
+
+function example(element) {
+    console.log("element clicked", element);
+}
+
+function turnOff(element) {
+    element.innerText = "Off";
+}
+
+function hide(element) {
+    element.remove();
+}
